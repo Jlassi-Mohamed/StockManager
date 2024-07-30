aee01b34450e8dd167af6ae19b1c122d74b8957f
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2024-07-30 10:08:25 +0100

    Create 'Home' view

---
+++ StockManager/views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+import accounts.serializers
+
+
+class Home(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_classes = accounts.serializers.MyTokenObtainPairSerializer
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_authenticated:
+            token = request.auth
+            if token is None:
+                return HttpResponse({
+                    "message": "Token is missing",
+                }, status=401)
+            token = token.get('token')
+            username = token.get('username')
+            role = token.get('role')
+            permissions = token.get('permissions')
+            return HttpResponse({
+                'username': username,
+                'role': role,
+                'permissions': permissions,
+                'token': token
+            })
+        return HttpResponseRedirect(reverse('accounts:login'))
+
+
